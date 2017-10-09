b8cdac4e92d63bdd062947d217deb4d94298ebf4
Lucidastar <lucidahxx@gmail.com>
2017-10-09 11:29:10 +0800

    添加了左侧返回按钮

--- app/src/main/java/com/lucidastar/hodgepodge/ui/activity/layout/CollapsingToolbarActivity.java
+++ app/src/main/java/com/lucidastar/hodgepodge/ui/activity/layout/CollapsingToolbarActivity.java
@@ -15,15 +15,21 @@ public class CollapsingToolbarActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collapsing_toolbar);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
     }
