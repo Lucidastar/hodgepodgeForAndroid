3103b735296fe55049a488195a8e9ae3d0a1bea5
Lucidastar <lucidahxx@gmail.com>
2017-10-20 11:44:29 +0800

    属性动画的更改

--- app/src/main/java/com/lucidastar/hodgepodge/ui/activity/otherfeature/animation/PropertyAnimActivity.java
+++ app/src/main/java/com/lucidastar/hodgepodge/ui/activity/otherfeature/animation/PropertyAnimActivity.java
@@ -1,5 +1,6 @@
 package com.lucidastar.hodgepodge.ui.activity.otherfeature.animation;
 
+import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
@@ -64,7 +65,7 @@ public class PropertyAnimActivity extends AppCompatActivity {
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                KLog.i("值的变化：" + animation.getAnimatedValue());
+                KLog.d("值的变化：" + animation.getAnimatedValue());
             }
         });
         valueAnimator.start();
@@ -77,18 +78,23 @@ public class PropertyAnimActivity extends AppCompatActivity {
             switch (menuItem.getItemId()) {
                 case R.id.item_alpha:
                     msg += "alpha";
+                    startAnimation(0);
                     break;
                 case R.id.item_rotation:
                     msg += "rotation";
+                    startAnimation(1);
                     break;
                 case R.id.translationX:
                     msg += "translationX";
+                    startAnimation(2);
                     break;
                 case R.id.item_scaleY:
                     msg += "scaleY";
+                    startAnimation(3);
                     break;
                 case R.id.item_all:
                     msg += "all";
+                    startAnimation(4);
                     break;
             }
 
@@ -101,10 +107,28 @@ public class PropertyAnimActivity extends AppCompatActivity {
 
 
     private void startAnimation(int pro) {
-        ObjectAnimator animator;
-//        if ()
-//        animator = ObjectAnimator.ofFloat(tvProperty, pro, 1f, 3f, 1f);
-//        animator.setDuration(5000);
-//        animator.start();
+        ObjectAnimator animator = null;
+        if (pro == 0) {
+            animator = ObjectAnimator.ofFloat(tvProperty, "alpha", 1f, 3f, 1f);
+        }else if (pro == 1){
+            animator = ObjectAnimator.ofFloat(tvProperty, "rotation", 0f, 360f);
+        }else if (pro == 2){
+            float curTranslationX = tvProperty.getTranslationX();
+            animator = ObjectAnimator.ofFloat(tvProperty, "translationX", curTranslationX, -500f, curTranslationX);
+        }else if (pro == 3){
+            animator = ObjectAnimator.ofFloat(tvProperty, "scaleY", 1f, 3f, 1f);
+        }else if (pro == 4){
+            ObjectAnimator moveIn = ObjectAnimator.ofFloat(tvProperty, "translationX", -500f, 0f);
+            ObjectAnimator rotate = ObjectAnimator.ofFloat(tvProperty, "rotation", 0f, 360f);
+            ObjectAnimator fadeInOut = ObjectAnimator.ofFloat(tvProperty, "alpha", 1f, 0f, 1f);
+            AnimatorSet animSet = new AnimatorSet();
+            animSet.play(rotate).with(fadeInOut).after(moveIn);
+            animSet.setDuration(5000);
+            animSet.start();
+        }
+        if (pro != 4) {
+            animator.setDuration(5000);
+            animator.start();
+        }
     }
 }
